6c8d57712f680ccc8cf1a04516ccc79509bf37ef
Fernando Daciuk <f.daciuk@gmail.com>
2016-08-19 22:44:08 -0300

    Update filter

--- first-steps/filter.hs
+++ first-steps/filter.hs
@@ -1,18 +1,18 @@
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
-filter f (x:xs) | f x       = x : filter f xs
-                | otherwise = filter f xs
+filter' f (x:xs) | f x       = x : filter' f xs
+                 | otherwise = filter' f xs
 
 
 -- TESTE DE MESA
 -- --------------------------------------------------
 -- Execução                    | Resultado
 -- --------------------------- | --------------------
--- filter (>3) [1, 2, 3, 4, 5] | [4, 5]
+-- filter' (>3) [1, 2, 3, 4, 5] | [4, 5]
 --                             |
--- filter (>3) [2, 3, 4, 5]    |
--- filter (>3) [3, 4, 5]       |
--- filter (>3) [4, 5]          |
--- 4:(filter (>3) [5])         |
--- 4:5:(filter (>3) []))       |
+-- filter' (>3) [2, 3, 4, 5]    |
+-- filter' (>3) [3, 4, 5]       |
+-- filter' (>3) [4, 5]          |
+-- 4:(filter' (>3) [5])         |
+-- 4:5:(filter' (>3) []))       |
 -- 4:5:[]                      | [4, 5]
